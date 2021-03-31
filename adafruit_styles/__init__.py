f172fd813c7d9be876af343aa51d284babd795b0
jposada202020 <jquintana202020@gmail.com>
2021-03-31 11:28:42 -0400

    # UPDATE API.rst # Update example # Create new class # docstring creation

--- adafruit_styles/__init__.py
+++ adafruit_styles/__init__.py
@@ -3,15 +3,44 @@
 # SPDX-License-Identifier: MIT
 
 """
-`adafruit_syles`
+`adafruit_styles`
 =======================
 """
 
 
 def apply_style(target, style):
     """
-    target: widget object
-    style: Style to apply
+    :param target: widget object
+    :param style: Style to apply
+
+
+    apply_style could be used in certain libraries that provide access to the color setter.
+
+
+    **Quickstart: Importing and using apply_style**
+
+    Here is one war of importing ``apply_style`` function so you can use it as
+    the name ``colors`` and the color set ``DarkAmber``:
+
+    .. code-block:: python
+
+        from Adafruit_CircuitPython_Styles import apply_style
+        from Adafruit_CircuitPython_Styles.styles import DarkAmber
+
+    For a created Label object in ``adafruit_display_text``
+
+    .. code-block:: python
+
+        text_area = bitmap_label.Label(terminalio.FONT, text="Hello", x=10, y=10)
+
+    you can apply the style library using:
+
+     .. code-block:: python
+
+        apply_style(text_area, DarkAmber)
+
+    The function will verify the object and apply the changes according to the features available
+
     """
     text_display = ["Label", "Bitmap_label"]
     annotation_widget = ["Annotation"]
--- examples/styles_simpletest.py
+++ examples/styles_simpletest.py
@@ -8,12 +8,13 @@ This example shows the use of library with an adafruit_display_text.label object
 import board
 import terminalio
 from adafruit_display_text import label
+from adafruit_styles import apply_style
 
 text = "Hello world"
 text_area = label.Label(terminalio.FONT, text=text)
 text_area.x = 10
 text_area.y = 10
-text_area.label_style = "LightGreen8"
+apply_style(text_area, "DarkBlue")
 board.DISPLAY.show(text_area)
 while True:
     pass
